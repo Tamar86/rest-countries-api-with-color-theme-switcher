605c0a1886ff7a432589c6890bbecbfc793dc043
Tamar86 <takkokikalishvili@gmail.com>
2024-11-10 21:57:21 +0000

    Adding HashRouter for deployment

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,17 @@
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, HashRouter } from 'react-router-dom';
 import Layout from './components/Layout';
 import { CountriesProvider } from './context/CountriesContext';
 
 function App() {
 	return (
 		<CountriesProvider>
-			<BrowserRouter basename='/rest-countries-api-with-color-theme-switcher'>
+			<HashRouter>
 				<Layout />
-			</BrowserRouter>
+			</HashRouter>
 		</CountriesProvider>
 	);
 }
 
 export default App;
+
+//basename='/rest-countries-api-with-color-theme-switcher'
